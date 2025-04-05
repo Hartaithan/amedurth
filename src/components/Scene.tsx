908fee192238d3458fc8430a672fc7f1ba20515b
Hartaithan <hartaithan@gmail.com>
2025-04-05 14:57:44 +0900

    install drei and setup environment with controls

--- src/components/Scene.tsx
+++ src/components/Scene.tsx
@@ -1,14 +1,16 @@
+import { Environment, OrbitControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import { FC } from "react";
 
 const Scene: FC = () => {
   return (
-    <Canvas id="scene">
-      <mesh>
+    <Canvas shadows>
+      <mesh castShadow>
         <boxGeometry args={[2, 2, 2]} />
-        <meshStandardMaterial wireframe />
+        <meshStandardMaterial />
       </mesh>
-      <ambientLight intensity={0.1} />
+      <Environment preset="sunset" background blur={1} />
+      <OrbitControls />
     </Canvas>
   );
 };
