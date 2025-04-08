cae513128efbfceaae259a7fed407f4cd4bee632
Hartaithan <hartaithan@gmail.com>
2025-04-08 16:04:25 +0900

    hide html element when geometry is rotating

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -13,8 +13,9 @@ const Card: FC<Props> = (props) => {
     <Html
       className="pointer-events-none flex justify-between gap-5 rounded bg-white/75 px-3 py-2 select-none"
       transform
-      position={[0, sizes.display[1] / 2 - 2, sizes.display[2] / 2]}
-      style={{ width: sizes.display[0] * 35 }}>
+      position={[0, sizes.display[1] / 2 - 2, sizes.display[2] / 2 + 0.01]}
+      style={{ width: sizes.display[0] * 35 }}
+      occlude>
       <div className="flex flex-col justify-center">
         <p>{item.title}</p>
         <p>{item.description}</p>
